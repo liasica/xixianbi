3b594a2ea9f9a600340f48b0d4f97a7ffe825d7e
liasica <magicrolan@qq.com>
2020-04-25 14:52:46 +0800

    add pages less

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,15 +1,24 @@
 <template>
     <div class="header">
         <div class="container">
-            <button class="menu"></button>
-            <div class="now s-btn">{{ now }}</div>
+            <div>
+                <button class="menu"></button>
+                <div class="now s-btn">{{ now }}</div>
+            </div>
             <div class="logo"></div>
+            <div>
+                <div class="switch-table">
+                    <button>切换表格</button>
+                    <button>关闭</button>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 import moment from 'moment'
+// import axios from 'axios'
 
 export default {
     data () {
@@ -17,10 +26,13 @@ export default {
             now: ''
         }
     },
-    created () {
+    async created () {
         setInterval(() => {
             this.now = moment().format('YYYY-MM-DD HH:mm:ss')
         }, 1000)
+
+        // const data = await axios.get('https://api.seniverse.com/v3/weather/now.json?key=SaQfcWF2mcKTuwIjk&location=34.26316071:108.94802094&language=zh-Hans&unit=c')
+        // console.info(data)
     }
 }
 </script>
---
+++ src/components/switchTable.vue
@@ -0,0 +1,3 @@
+<template>
+    <div class="switch-table"></div>
+</template>
