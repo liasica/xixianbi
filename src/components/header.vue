9de80d8a6fe87466ca1ca365e8ec7b1dfe2b6f3d
liasica <magicrolan@qq.com>
2020-04-25 14:06:12 +0800

    header

--- src/components/header.vue
+++ src/components/header.vue
@@ -2,7 +2,25 @@
     <div class="header">
         <div class="container">
             <button class="menu"></button>
+            <div class="now s-btn">{{ now }}</div>
             <div class="logo"></div>
         </div>
     </div>
 </template>
+
+<script>
+import moment from 'moment'
+
+export default {
+    data () {
+        return {
+            now: ''
+        }
+    },
+    created () {
+        setInterval(() => {
+            this.now = moment().format('YYYY-MM-DD HH:mm:ss')
+        }, 1000)
+    }
+}
+</script>
