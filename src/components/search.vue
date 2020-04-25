49b8acef37a3b06a4060bc0cca49eedd80289845
liasica <magicrolan@qq.com>
2020-04-25 16:15:54 +0800

    add vscode workspace configure

--- src/components/search.vue
+++ src/components/search.vue
@@ -2,6 +2,7 @@
     <s-btn corner="#42DFFF">
         <div class="searcher">
             <input type="search" placeholder="搜索公交" />
+            <button class="btn-search"></button>
         </div>
     </s-btn>
 </template>
