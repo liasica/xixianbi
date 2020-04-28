212a528d11ea3dbcd80fcd6596749eff013a5d85
tonokim <tonokim@qq.com>
2020-04-28 09:18:55 +0800

    station

---
+++ src/components/tieText.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="tie-text">
+        <s-btn class="content">
+            <slot></slot>
+        </s-btn>
+    </div>
+</template>
+<style lang="less" scoped>
+.tie-text {
+    position: relative;
+    padding: 4px;
+    height: 54px;
+    &:before,
+    &:after {
+        position: absolute;
+        content: '';
+        width: 100%;
+        height: 12px;
+        left: 0;
+        border: 1px solid #42dfff;
+    }
+    &:before {
+        top: 0;
+        border-bottom: none;
+    }
+    &:after {
+        bottom: 0;
+        border-top: none;
+    }
+    .content {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+}
+</style>
--- src/views/station/view.vue
+++ src/views/station/view.vue
@@ -1,11 +1,125 @@
 <template>
     <div class="container station-container">
-        <img :src="require('@images/demo/6.png')" class="demo" />
+        <div class="bi-title">场站信息</div>
+        <TieText class="sub-title">场站一</TieText>
+        <div class="violation">
+            <div class="bi-title">当日车辆违规统计</div>
+            <ul>
+                <li class="thin-border border-bottom">
+                    <img class="icon-users" :src="require('./assets/users.png')" />
+                    <span>本月30内报警数</span>
+                    <div class="value">100</div>
+                </li>
+                <li class="thin-border border-bottom">
+                    <img class="icon-driver" :src="require('./assets/driver.png')" />
+                    <span>驾驶员违法违规</span>
+                    <div class="value">80</div>
+                </li>
+                <li class="thin-border border-bottom">
+                    <img class="icon-user" :src="require('./assets/user.png')" />
+                    <span>驾驶员事故次数</span>
+                    <div class="value">60</div>
+                </li>
+            </ul>
+        </div>
+        <div class="bi-title">累计进出量</div>
+        <div class="progress-bars">
+            <progress-bar class="pbar" label="常规公交" :schedule="getRandomInt(100)" />
+            <progress-bar class="pbar" label="定制公交" :schedule="getRandomInt(100)" color="#08F0C9" />
+            <progress-bar class="pbar" label="通勤公交" :schedule="getRandomInt(100)" color="#3C77FF" />
+        </div>
+        <div class="bi-title">充电桩规模</div>
+        <div class="charge-places">
+            <img src="./assets/charge-places.png" alt />
+            <p>电桩类别：10/公交 20/公共</p>
+            <p>所属运营商： BYD</p>
+            <p>累计充电时间： 44.5小时</p>
+        </div>
     </div>
 </template>
+<script>
+import TieText from '@/components/tieText'
+import ProgressBar from '@/components/progressBar'
+
+export default {
+    components: {
+        TieText,
+        ProgressBar
+    },
+    methods: {
+        getRandomInt (max) {
+            return Math.floor(Math.random() * Math.floor(max))
+        }
+    }
+}
+</script>
 
 <style lang="less" scoped>
+.station-container {
+    display: flex;
+    flex-direction: column;
+}
 .demo {
     width: 330px;
+    position: absolute;
+}
+.sub-title {
+    width: 260px;
+    margin-left: 20px;
+}
+.violation {
+    margin-top: 24px;
+    width: 330px;
+    li {
+        display: flex;
+        align-items: center;
+        padding-bottom: 14px;
+        margin-bottom: 14px;
+        .value {
+            flex: 1;
+            text-align: right;
+            opacity: 0.9;
+            font-size: 28px;
+        }
+        span {
+            opacity: 0.8;
+        }
+    }
+    img {
+        width: 100%;
+        height: 100%;
+    }
+    .icon-users {
+        width: 34px;
+        height: 24px;
+        margin-right: 12px;
+    }
+    .icon-driver {
+        width: 30px;
+        height: 32px;
+        margin-right: 12px;
+    }
+    .icon-user {
+        width: 28px;
+        height: 34px;
+        margin-right: 12px;
+    }
+}
+.progress-bars {
+    width: 330px;
+}
+.charge-places {
+    // width: 330px;
+    position: relative;
+    padding-left: 100px;
+    img {
+        position: absolute;
+        width: 120px;
+        left: 0;
+    }
+    p {
+        margin: 15px 0;
+        font-size: 18px;
+    }
 }
 </style>
